e98392033801eb1e419aa24efb0904451132ef08
lalalalala <tomato.stao@gmail.com>
2021-11-25 20:56:22 +0800

    :sparkles: 30

---
+++ advanced/30_unsaferust/examples/ptr.rs
@@ -0,0 +1,18 @@
+fn main() {
+  let mut age = 18;
+
+  let r1 = &age as *const i32;
+  let r2 = &mut age as *mut i32;
+
+  unsafe {
+    println!("r1: {}, r2: {}", *r1, *r2);
+  }
+}
+
+// fn immutable_mutable_cant_coexist() {
+//   let mut  age = 18;
+//   let r1 = &age;
+//   let r2 = &mut &mut age;
+
+//   println!("r1: {}, r2: {}", r1, r2);
+// }
