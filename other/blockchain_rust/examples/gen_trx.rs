ccb522dc228197717e487a6611bca56968e2c7ce
duolong <tomato.stao@gmail.com>
2022-10-03 17:50:08 +0800

    :ambulance: add wallet

--- other/blockchain_rust/examples/gen_trx.rs
+++ other/blockchain_rust/examples/gen_trx.rs
@@ -1,13 +1,15 @@
 use std::{env::current_dir, sync::Arc};
 
-use blockchain_rust_part::{Blockchain, SledDb, Transaction, UTXOSet};
+use blockchain_rust_part::{Blockchain, SledDb, Transaction, UTXOSet, Wallets};
 
 fn main() {
     tracing_subscriber::fmt().init();
 
-    let justin_addr = "Justin";
-    let bob_addr = "Bob";
-    // let bruce_addr = "Bruce";
+    let justin_addr = "16pJjWzDQshWjUaqopugU7jc9CRbBN3oFQ";
+
+    let mut wallets = Wallets::new().unwrap();
+    let bob_addr = wallets.create_wallet();
+    let bruce_addr = wallets.create_wallet();
 
     let path = current_dir().unwrap().join("other/blockchain_rust/data");
     let storage = Arc::new(SledDb::new(path));
@@ -15,10 +17,10 @@ fn main() {
     let mut bc = Blockchain::new(storage.clone(), justin_addr);
     let utxos = UTXOSet::new(storage);
 
-    let tx_1 = Transaction::new_utxo(justin_addr, bob_addr, 4, &utxos);
-    // let tx_2 = Transaction::new_utxo(justin_addr, bruce_addr, 2, &utxos);
+    let tx_1 = Transaction::new_utxo(justin_addr, &bob_addr, 4, &utxos, &bc);
+    let tx_2 = Transaction::new_utxo(justin_addr, &bruce_addr, 2, &utxos, &bc);
 
-    let txs = vec![tx_1];
+    let txs = vec![tx_1, tx_2];
 
     bc.mine_block(&txs);
     utxos.reindex(&bc).unwrap();
