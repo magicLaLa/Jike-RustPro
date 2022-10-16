92e851c6b88d7fdb65334ef4e221398c6d3fe127
duolong <tomato.stao@gmail.com>
2022-10-16 10:17:26 +0800

    :sparkles: 静态动态分发

--- other/builder-lite/src/main.rs
+++ other/builder-lite/src/main.rs
@@ -30,8 +30,53 @@ impl Shape {
     }
 }
 
+trait Processor {
+    fn compute(&self, x: i64, y: i64) -> i64;
+}
+
+struct Risc {}
+
+impl Processor for Risc {
+    fn compute(&self, x: i64, y: i64) -> i64 {
+        x + y
+    }
+}
+
+struct Cisc {}
+
+impl Processor for Cisc {
+    fn compute(&self, x: i64, y: i64) -> i64 {
+        x + y
+    }
+}
+
+/// 静态分发，编译时确定大小
+fn process(processor: impl Processor, x: i64) {
+    let result = processor.compute(x, 42);
+    println!("result: {}", result);
+}
+/// 动态分发，编译时只根据 指针来，因为指针大小是确定的，只有在运行时找到一对应方法或者函数调用
+fn process2(processor: Box<dyn Processor>, x: i64) {
+    let result = processor.compute(x, 42);
+    println!("result: {}", result);
+}
+
 fn main() {
     let shape = Shape::new(1.4).with_test(4.6).with_postion(vec![2, 4]);
     Shape::to_sss();
     println!("shape {:#?}", shape);
+
+    let processor1 = Risc {};
+    let processor2 = Cisc {};
+
+    process(processor1, 1);
+    process(processor2, 2);
+
+    let processors: Vec<Box<dyn Processor>> = vec![
+        Box::new(Risc {}), Box::new(Cisc {})
+    ];
+
+    for processor in processors {
+        process2(processor, 1);
+    }
 }
