850371853a2fb238b39cfe44d39a9e92f39637e4
duolong <tomato.stao@gmail.com>
2022-11-24 16:55:28 +0800

    :sparkles: &mut &[T]

--- other/concurrent_example/src/lib.rs
+++ other/concurrent_example/src/lib.rs
@@ -3,9 +3,15 @@ pub fn is_prime(n: u32) -> bool {
     (2..=n / 2).all(|i| n % i != 0)
 }
 
+#[derive(Debug)]
+struct Test {
+    r#in: String,
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::io::Read;
 
     #[test]
     fn test_first_hundred_primes() {
@@ -17,5 +23,44 @@ mod tests {
                 83, 89, 97
             ]
         )
+    }
+
+    #[test]
+    fn test_mut() {
+        // &mut &[T] 的意思
+        // let value = 1;
+        // let mut shared: &u32 = &value;
+        // println!("{r:p}: {r} (value = {v})", r = shared, v = value);
+
+        // let unique: &mut &u32 = &mut shared;
+        // *unique = &17;
+        // println!("{r:p}: {r} (value = {v})", r = shared, v = value);
+
+        // let mut slice: &[u8] = &[0, 1, 2, 3, 4];
+        // let umique = &mut slice;
+        // println!("{r:p}: {len}: {r:?}", r = *umique, len = umique.len());
+
+        // *umique = &umique[..4];
+        // println!("{r:p}: {len}: {r:?}", r = *umique, len = umique.len());
+
+        // *umique = &umique[1..];
+        // println!("{r:p}: {len}: {r:?}", r = *umique, len = umique.len());
+
+        // *umique = &[17, 17, 42];
+        // println!("{r:p}: {len}: {r:?}", r = *umique, len = umique.len());
+
+        // println!("{r:p}: {len}: {r:?}", r = slice, len = slice.len());
+
+        let mut data: &[u8] = &[0, 1, 2, 3, 4, 5, 6];
+        let mut buf = [0; 3];
+
+        while let Ok(1..) = Read::read(&mut data, &mut buf) {
+            println!("{r:p}: {len}: {r:?}", r = data, len = data.len());
+        }
+
+        let test = Test {
+            r#in: "sadf".to_string(),
+        };
+        println!("{:?}", test);
     }
 }
